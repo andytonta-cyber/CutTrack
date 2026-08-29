c6b519d58562ce719badbab7fb5249c585b0436b
andytonta-cyber <andy.tonta@hotmail.com>
2026-08-29 12:44:44 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const CACHE = "cuttrack-v3";
-const ASSETS = [
+const CACHE = "cuttrack-v5";
+
+const CORE = [
   "./",
   "./index.html",
   "./style.css",
@@ -9,34 +10,62 @@ const ASSETS = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+    caches
+      .open(CACHE)
+      .then(cache => cache.addAll(CORE))
   );
+
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE)
-          .map(key => caches.delete(key))
+    caches
+      .keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+        )
       )
-    )
   );
+
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  const url = new URL(event.request.url);
+
+  if (url.origin !== self.location.origin) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
   event.respondWith(
-    fetch(event.request)
+    fetch(event.request, {
+      cache: "no-store"
+    })
       .then(response => {
         const copy = response.clone();
-        caches.open(CACHE).then(cache => {
-          cache.put(event.request, copy);
-        });
+
+        caches
+          .open(CACHE)
+          .then(cache =>
+            cache.put(
+              event.request,
+              copy
+            )
+          );
+
         return response;
       })
-      .catch(() => caches.match(event.request))
+      .catch(() =>
+        caches.match(
+          event.request
+        )
+      )
   );
 });
